ec30b8fa5b8c1803674ce5844401b186e4a9c9b7
hydroft1 <marquetalexandre@proton.me>
2024-11-22 10:16:58 +0100

    FICHIER LAUNCH ET TASK FONCTIONNE

---
+++ exo0PriseEnMain/v2/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Fichier : main.cpp
+ * But : Programme de prise en main de la notion de module
+ *       v0 : le programme complet (sous-programme et main) implanté dans 1 seul fichier
+ * Auteur : Alexandre.M
+ * date : 22-11-2024
+ * Référence : feuille de TD n°1, exercice 0
+ */
+
+#include "module.h"
+#include <iostream>
+using namespace std;
+
+
+int main (void)
+{
+    cout << "debut de main() " << endl;
+    sp1();
+    sp2();
+    cout << "fin de main() " <<endl;
+    return 0;
+}
+
+
---
+++ exo0PriseEnMain/v2/module.cpp
@@ -0,0 +1,13 @@
+// module.cpp
+#include "module.h"
+#include <iostream>
+
+void sp1()
+{
+    std::cout << "sous-programme 1" << std::endl;
+}
+
+void sp2()
+{
+    std::cout << "sous-programme 2" << std::endl;
+}
